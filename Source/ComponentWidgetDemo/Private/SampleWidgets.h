f5a984fdff416508a79f621b1be8bcde4526a759
korimart <korimart2019@gmail.com>
2024-01-24 11:59:11 +0900

    접기 열기 구현

--- Source/ComponentWidgetDemo/Private/SampleWidgets.h
+++ Source/ComponentWidgetDemo/Private/SampleWidgets.h
@@ -3,28 +3,77 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "ClickListenerUWComponent.h"
+#include "CollapserUWComponent.h"
+#include "ComponentUserWidget.h"
+#include "TextUWComponent.h"
 #include "Blueprint/UserWidget.h"
 #include "SampleWidgets.generated.h"
 
 /**
  * 
  */
-// UCLASS()
-// class USimpleButton : public UKmUserWidget
-// {
-// 	GENERATED_BODY()
-//
-// public:
-// 	UPROPERTY(VisibleAnywhere)
-// 	UKmClickListener* ClickListener; 
-// 	
-// 	USimpleButton()
-// 	{
-// 		ClickListener = CreateDefaultSubobject<UKmClickListener>(TEXT("ClickListener"));
-// 	}
-// };
-//
-//
+UCLASS()
+class USimpleButton : public UComponentUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere)
+	UTextUWComponent* TextComponent;
+	
+	UPROPERTY(VisibleAnywhere)
+	UClickListenerUWComponent* ClickListener;
+	
+	USimpleButton()
+	{
+		TextComponent = CreateDefaultSubobject<UTextUWComponent>(TEXT("TextComponent"));
+		ClickListener = CreateDefaultSubobject<UClickListenerUWComponent>(TEXT("ClickListener"));
+	}
+
+	// UComponentUserWidget
+	virtual void InitializeComponent() override
+	{
+		TextComponent->Init(Text);
+	}
+	// ~UComponentUserWidget
+
+private:
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* Text;
+};
+
+
+/**
+ * 
+ */
+UCLASS()
+class USimpleFoldable : public UComponentUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere)
+	UCollapserUWComponent* Collapser; 
+	
+	USimpleFoldable()
+	{
+		Collapser = CreateDefaultSubobject<UCollapserUWComponent>(TEXT("Collapser"));
+	}
+
+	// UComponentUserWidget
+	virtual void InitializeComponent() override
+	{
+		Collapser->Init(SizeBox);
+	}
+	// ~UComponentUserWidget
+
+private:
+	UPROPERTY(meta=(BindWidget))
+	USizeBox* SizeBox;
+};
+
+
 // /**
 //  *
 //  */
@@ -94,10 +143,38 @@
 // 	UPROPERTY(EditAnywhere)
 // 	bool bHorizontal = false;
 // };
-//
-//
+
+
 UCLASS()
 class UMainWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	// UUserWidget
+	virtual void NativeOnInitialized() override
+	{
+		Super::NativeOnInitialized();
+
+		ExpandLoremButton->ClickListener->OnClicked.AddWeakLambda(this, [this]()
+		{
+			LoremLipsum->Collapser->Expand();
+		});
+		
+		CollapseLoremButton->ClickListener->OnClicked.AddWeakLambda(this, [this]()
+		{
+			LoremLipsum->Collapser->Collapse();
+		});
+	}
+	// ~UUserWidget
+
+private:
+	UPROPERTY(meta=(BindWidget))
+	USimpleFoldable* LoremLipsum;
+	
+	UPROPERTY(meta=(BindWidget))
+	USimpleButton* ExpandLoremButton;
+	
+	UPROPERTY(meta=(BindWidget))
+	USimpleButton* CollapseLoremButton;
 };
